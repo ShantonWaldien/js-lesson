068b4f2f28a0561f20f41e86012908562ab97b59
ShantonWaldien <waldienshanton@gmail.com>
2018-11-07 11:48:49 +0200

    Created a paragraph in the console log using js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js-lesson
 First JavaScript lesson with lecturer.
+
+## Live Link
+https://shantonwaldien.github.io/js-lesson/index.html
---
+++ js/main.js
@@ -0,0 +1,57 @@
+'use strict'; 
+// ‘use strict’ – The ‘use strict’ directive switches the engine to the “modern” mode, changing the behaviour of some built In features.
+
+
+// Variables are used to store this information.
+
+
+// A variable is a "named storage" for data. We can use variables to store data.
+
+// To create a variable in JavaScript, we need to use the "let" or "var" keyword.
+
+var message
+
+// assingment operator is the  " = " sign.
+
+// We add a string " "
+// A string in JavaScript is a set of characters surrounded by quotation marks.
+
+// var message = "Good morning";
+//     // message = "How are you today?"
+// alert(message);
+
+// var name = "Shanton",
+//     age = "20",
+//     gender = "male";
+// console.log(name , age , gender);
+
+var name = prompt("Enter your first name:")
+alert("Hello," + name);
+
+// Choose meaningful names
+// Dont use reserved words
+// Folow conventions
+
+/******************************* *********************************/
+
+// We will now look at conditional execution.
+
+var name = "Shanton Waldien.",
+    age = "20",
+    gender = "male",
+    profession = " Junior Web Developer.",
+    institution = "Salesian Life Choices",
+    duration = "6 months.",
+    company = "Google",
+    future = "8 years.";
+    name = "Zainab Alexander.",
+    age = "21",
+    gender = "female",
+
+
+
+
+console.log("My name is " + name + " I am a "+ age + " year old " + gender + profession + " I have studied at " + institution + " for a duration of " + duration + " I would like to be working at " + company + " within the next " + future);
+
+
+
