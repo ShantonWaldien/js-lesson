c574f657b8a9d9a6b62d91b6f7f2cbcbbe5adfda
ShantonWaldien <waldienshanton@gmail.com>
2019-01-22 10:55:49 +0200

    doing a bit of back end js

---
+++ js/backend.js
@@ -0,0 +1,15 @@
+// Request data from a backend
+
+// Put simply, HTTP is a protocol which allows us to send information around the web. For example, When your browser asks for http://domain. HTTP requests come in different flavors, using different verbs (GET, POST, PUT and DELETE are the four most common). THe kidn of request your browser makes (and the kind we will use in this chapter) is a GET request. A GET request
+
+// AJAX stands for Asynchrous JavaScript and XML.Now theres a mouthful.
+
+// Basically , it is a system that allows us to send reuqests and handle the responses from within our JavaScript code. For example, when you leave a comment on a blog post, when you send or recieve email on a webmail service like Gmail, or whe a webpage updates itself without refreshing, It is often using AJAX to do so.
+
+// Two other initialisms you may see in this context are API and JSON.
+
+    // API stands for Application Programming Interface. Many companies and services provide APIs which will react in specific ways when we send them HTTP requests. You will come across a few in this course.
+
+    // JSON, or JavaScript Object Notation is a way of representing and handling data as a JavaScript object. It is extremely useful for sending and recieving information
+
+
